1b1fa2dc9bc7764e662e96ae664b554850c03d6b
William T Cao <williamacao@gmail.com>
2020-08-22 17:49:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,7 +33,7 @@ end
 
 def retrieve_first_element_from_array(array)
   names = ["wow", "Sidney", "Justin", "Stephanie"]
-  names.index("wow")
+  names.index [0]
 end
 
 def retrieve_last_element_from_array(array)
