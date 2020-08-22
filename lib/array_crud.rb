314de25754bb744dc07161ca07152b6aef56cd55
William T Cao <williamacao@gmail.com>
2020-08-22 17:27:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  names, element << "Lucy"
+  names << "Lucy"
+  element << "Lucy"
 end
 
 def add_element_to_start_of_array(array, element)
