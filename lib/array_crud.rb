31a5358bcea67a633bdaa7f32126d089fe7a0ddd
William T Cao <williamacao@gmail.com>
2020-08-22 17:47:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  names = ["William", "Sidney", "Justin", "Stephanie"]
+  names = ["William", "Sidney","am", "Stephanie"]
   names[2]
 end
 
