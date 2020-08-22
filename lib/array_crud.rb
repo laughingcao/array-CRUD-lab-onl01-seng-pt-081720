6f9d02ce6b64ff8f715ecebf51ebff1af6d7cbaa
William T Cao <williamacao@gmail.com>
2020-08-22 17:42:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  names = ["William", "Sidney", "Justin", "Stephanie"]
   names.pop
 end
 
