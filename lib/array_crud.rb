0ab70acf6c0d20cf94ba68eaca20265c2edbe0d8
William T Cao <williamacao@gmail.com>
2020-08-22 17:42:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   names = ["William", "Sidney", "Justin", "Stephanie"]
-  names.unshift "arrays!"
+  names.unshift "array!"
 end
 
 def remove_element_from_end_of_array(array)
